70d4db58f367fb63b81b4bbf7d91bc98bad2ea54
Jared Hanson <jaredhanson@gmail.com>
2011-09-15 22:30:46 -0700

    Document Client class.

--- test/client-test.js
+++ test/client-test.js
@@ -1,7 +1,7 @@
 var vows = require('vows');
 var assert = require('assert');
-var util = require('util');
 var junction = require('junction');
+var util = require('util');
 var Client = require('junction-disco/client');
 var InfoQuery = require('junction-disco/elements/infoquery');
 var ItemsQuery = require('junction-disco/elements/itemsquery');
@@ -11,24 +11,21 @@ vows.describe('Client').addBatch({
   
   'initialization': {
     topic: function() {
-      return new Client({ jid: 'catalog.shakespeare.lit' });
+      return new Client({ jid: 'catalog.shakespeare.lit', disableStream: true });
     },
     
-    'should have an empty stack': function (c) {
-      assert.length(c._stack, 0);
-    },
     'should have an info function': function (c) {
       assert.isFunction(c.info);
     },
     'should have an items function': function (c) {
-      assert.isFunction(c.info);
+      assert.isFunction(c.items);
     },
   },
   
   'receiving an info query': {
     topic: function() {
       var self = this;
-      var client = new Client({ jid: 'catalog.shakespeare.lit' });
+      var client = new Client({ jid: 'catalog.shakespeare.lit', disableStream: true });
       client.info(null, function(req, res, next) {
         self.callback(null, req);
       });
@@ -49,7 +46,7 @@ vows.describe('Client').addBatch({
   'receiving an items query': {
     topic: function() {
       var self = this;
-      var client = new Client({ jid: 'catalog.shakespeare.lit' });
+      var client = new Client({ jid: 'catalog.shakespeare.lit', disableStream: true });
       client.items(null, function(req, res, next) {
         self.callback(null, req);
       });
