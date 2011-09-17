5d1dd68450809f7319ce1d092a0faaddacbcda9e
Jared Hanson <jaredhanson@gmail.com>
2011-09-17 00:37:02 -0700

    Test route param captures.

--- test/client-test.js
+++ test/client-test.js
@@ -228,4 +228,53 @@ vows.describe('Client').addBatch({
     },
   },
   
+  'routing a query to a node with a capture': {
+    topic: function() {
+      var self = this;
+      var client = new Client({ jid: 'catalog.shakespeare.lit', disableStream: true });
+      client.items('/people/:username', function(req, res, next) {
+        self.callback(null, req);
+      });
+      process.nextTick(function () {
+        var iq = new junction.elements.IQ('catalog.shakespeare.lit', 'romeo@montague.net/orchard', 'get');
+        var query = new ItemsQuery('/people/johndoe');
+        iq.id = '1';
+        iq.c(query);
+        client.emit('stanza', iq.toXML());
+      });
+    },
+    
+    'should dispatch a request': function (err, stanza) {
+      assert.isNotNull(stanza);
+    },
+    'should set params': function (err, stanza) {
+      assert.equal(stanza.params.username, 'johndoe');
+    },
+  },
+  
+  'routing a query to a node with a multiple captures': {
+    topic: function() {
+      var self = this;
+      var client = new Client({ jid: 'catalog.shakespeare.lit', disableStream: true });
+      client.items('/music/:artist/tracks/:song', function(req, res, next) {
+        self.callback(null, req);
+      });
+      process.nextTick(function () {
+        var iq = new junction.elements.IQ('catalog.shakespeare.lit', 'romeo@montague.net/orchard', 'get');
+        var query = new ItemsQuery('/music/counting-crows/tracks/mr-jones');
+        iq.id = '1';
+        iq.c(query);
+        client.emit('stanza', iq.toXML());
+      });
+    },
+    
+    'should dispatch a request': function (err, stanza) {
+      assert.isNotNull(stanza);
+    },
+    'should set params': function (err, stanza) {
+      assert.equal(stanza.params.artist, 'counting-crows');
+      assert.equal(stanza.params.song, 'mr-jones');
+    },
+  },
+  
 }).export(module);
